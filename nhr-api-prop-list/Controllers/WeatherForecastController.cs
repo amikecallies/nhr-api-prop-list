5148dbd66abc0fee1eafa2bd461ee8ac3f69d16a
amikecallies <acallies15@apu.edu>
2020-08-31 12:48:37 -0700

    another initial commit for source control testing

--- nhr-api-prop-list/Controllers/WeatherForecastController.cs
+++ nhr-api-prop-list/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace nhr_api_prop_list.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Hot", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
